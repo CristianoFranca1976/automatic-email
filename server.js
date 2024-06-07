bec9dc959d29188dee23ec20029916f5a69e77da
CrstianoFranca1976 <cristiano.franca@hotmail.co.uk>
2024-06-07 14:10:10 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,98 @@
+// require('dotenv').config();
+// const express = require('express');
+// const nodemailer = require('nodemailer');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+// app.use(bodyParser.json());
+// app.use(express.static('public')); // Servir arquivos estáticos da pasta public
+
+// app.post('/send-email', async (req, res) => {
+//     const { name, email } = req.body;
+
+//     let transporter = nodemailer.createTransport({
+//         service: 'hotmail',
+//         auth: {
+//             user: process.env.EMAIL_USER,
+//             pass: process.env.EMAIL_PASS
+//         }
+//     });
+
+//     let mailOptions = {
+//         from: process.env.EMAIL_USER,
+//         to: email,
+//         subject: 'Obrigado pelo seu contato',
+//         text: `Olá ${name},\n\nObrigado por entrar em contato conosco. Em breve retornaremos o seu contato.\n\nAtenciosamente,\nEquipe`
+//     };
+
+//     try {
+//         await transporter.sendMail(mailOptions);
+//         res.status(200).send('Email enviado com sucesso.');
+//     } catch (error) {
+//         console.error('Erro ao enviar e-mail:', error);
+//         res.status(500).send('Erro ao enviar e-mail.');
+//     }
+// });
+
+// const PORT = process.env.PORT || 3021;
+// app.listen(PORT, () => {
+//     console.log(`Servidor rodando na porta ${PORT}`);
+// });
+
+require('dotenv').config();
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static('public')); // Servir arquivos estáticos da pasta public
+
+app.post('/send-email', async (req, res) => {
+    const { name, email } = req.body;
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp-mail.outlook.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    // Opções de e-mail para o cliente
+    let clientMailOptions = {
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: 'Obrigado pelo seu contato',
+        text: `Olá ${name},\n\nObrigado por entrar em contato conosco. Em breve retornaremos o seu contato.\n\nAtenciosamente,\nEquipe`
+    };
+
+    // Opções de e-mail para você
+    let adminMailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_USER, // Ou outro endereço de e-mail
+        subject: 'Novo feedback/contato recebido',
+        text: `Você recebeu um novo feedback/contato.\n\nNome: ${name}\nEmail: ${email}\n\nPor favor, verifique e responda adequadamente.`
+    };
+
+    try {
+        // Enviar e-mail para o cliente
+        await transporter.sendMail(clientMailOptions);
+        
+        // Enviar e-mail para o administrador
+        await transporter.sendMail(adminMailOptions);
+
+        res.status(200).send('Email enviado com sucesso.');
+    } catch (error) {
+        console.error('Erro ao enviar e-mail:', error);
+        res.status(500).send('Erro ao enviar e-mail.');
+    }
+});
+
+const PORT = process.env.PORT || 3021;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
